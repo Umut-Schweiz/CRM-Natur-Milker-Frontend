41ca5d1c04c69fc45bf143b051f938ac2157997c
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-08-26 23:52:16 +0200

    contact us created

--- crm-frontend/src/components/pages/ContactUs.js
+++ crm-frontend/src/components/pages/ContactUs.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import '../../App.css';
+import "./ContactUs.css";
+import { Container, Row ,  Col , Form  } from 'react-bootstrap';
 
-const ContactUs = () =>  {
+const ContactUs = () => {
 
   return (
-    <h1 className='contact-us'>Contact Us</h1>
+
+    <div>
+      <Container fluid="md">
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+
+
+    </div>
+
   )
 }
 
