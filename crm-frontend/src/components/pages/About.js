d94e5b46866f6efcb16e83b66b0d4bb40987583b
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-08-27 23:59:41 +0200

    about page completed

--- crm-frontend/src/components/pages/About.js
+++ crm-frontend/src/components/pages/About.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import '../../App.css';
+import './About.css';
 
-const About = () =>  {
+const About = () => {
 
-  return (
-    <h1 className='about'>ABOUT</h1>
-  )
+    return (
+        <div className="about-body">
+            <div className="about-section">
+                <div className="inner-container">
+                    <h1>About Us</h1>
+                    <p className="text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum, eligendi nostrum pariatur necessitatibus eius dicta a voluptates sit deleniti autem error eos totam nisi neque voluptates sit deleniti autem error eos totam nisi neque.
+                    </p>
+                    <div className="skills">
+                        <span>Web Design</span>
+                        <span>Photoshop & Illustrator</span>
+                        <span>Coding</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default About
