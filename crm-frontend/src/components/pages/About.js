9fee0b8dd4f9b5c467c5f15713ca2f301183aca6
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-08-28 23:53:52 +0200

    about page styled

--- crm-frontend/src/components/pages/About.js
+++ crm-frontend/src/components/pages/About.js
@@ -12,9 +12,7 @@ const About = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum, eligendi nostrum pariatur necessitatibus eius dicta a voluptates sit deleniti autem error eos totam nisi neque voluptates sit deleniti autem error eos totam nisi neque.
                     </p>
                     <div className="skills">
-                        <span>Web Design</span>
-                        <span>Photoshop & Illustrator</span>
-                        <span>Coding</span>
+                       <button className="about-read-me-button">Read Me</button>
                     </div>
                 </div>
             </div>
