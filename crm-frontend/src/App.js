e72b40f70b8ad83e41338da8164535f5884a9be5
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-08-31 01:05:55 +0200

    signup page created with validation functions

--- crm-frontend/src/App.js
+++ crm-frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import ContactUs from './components/pages/ContactUs';
-import SignUp from './components/pages/SignUp';
+import Form from './components/pages/SignUp/Form';
 import Login from './components/pages/Login';
 import MyAccount from './components/pages/MyAccount';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -21,7 +21,7 @@ function App() {
       <Route path='/' exact component={Home} />
       <Route path='/about' component={About} />
       <Route path='/contact-us' component={ContactUs} />
-      <Route path='/sign-up' component={SignUp} />
+      <Route path='/sign-up' component={Form} />
       <Route path='/login' component={Login} />
       <Route path='/my-account' component={MyAccount} />
     </Switch>
