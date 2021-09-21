be388e08d9d327d8a17eaad2f1007aca3d8e1c88
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-09-21 23:49:35 +0200

    myaccount page created

--- crm-frontend/src/pages/about/About.js
+++ crm-frontend/src/pages/about/About.js
@@ -1,12 +1,22 @@
+
 import React from 'react';
 import './About.css';
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
-
-
+import Swal from 'sweetalert2'
 
 const About = () => {
 
+    const onClick = () => {
+
+        Swal.fire({
+            title: 'Who are we?',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus',
+            icon: 'info',
+            confirmButtonText: 'Continue'
+          })
+    }
+    
     return (
         <>
         <Navbar/>
@@ -18,7 +28,7 @@ const About = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum, eligendi nostrum pariatur necessitatibus eius dicta a voluptates sit deleniti autem error eos totam nisi neque voluptates sit deleniti autem error eos totam nisi neque.
                     </p>
                     <div className="skills">
-                       <button className="about-read-me-button">Read Me</button>
+                       <button className="about-read-me-button" onClick={onClick} >Read Me</button>
                     </div>
                 </div>
             </div>
--- crm-frontend/src/pages/myaccount/MyAccount.js
+++ crm-frontend/src/pages/myaccount/MyAccount.js
@@ -1,16 +1,102 @@
 import React from 'react';
-import '../../App.css';
-import Navbar from '../../components/navbar/Navbar';
-import Footer from '../../components/footer/Footer';
+import "./MyAccount.css"
+
 
 const MyAccount = () =>  {
 
   return (
 
     <>
-    <Navbar/>
-    <h1 className='my-account'>My Account</h1>
-    <Footer/>
+      <div className="sidebar">
+        <div className="sidebar-brand">
+          <h2><i class="fas fa-user"></i><span> NaturMilker</span></h2>
+        </div>
+
+        <div className="sidebar-menu">
+        <ul>
+          <li>
+            <a href="./"> <i class="fas fa-user"></i>Dashboard</a>
+          </li>
+          <li>
+            <a href="./"> <span><i class="fas fa-user"></i></span>Costumers</a>
+          </li>
+          <li>
+            <a href="./"> <span><i class="fas fa-user"></i></span>Projects</a>
+          </li>
+          <li>
+            <a href="./"> <span><i class="fas fa-user"></i></span>Orders</a>
+          </li>
+          <li>
+            <a href="./"> <span><i class="fas fa-user"></i></span>Inventory</a>
+          </li>
+          <li>
+            <a href="./"> <span><i class="fas fa-user"></i></span>Accounts</a>
+          </li>
+          <li>
+            <a href="./"> <span><i class="fas fa-user"></i></span>Tasks</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+
+      
+
+      <div className="main-content">
+        <header>
+          <div className="header-title">
+            <h3>
+            <label for="">
+              <span><i class="fas fa-user"></i></span>
+            </label>
+            Dashboard
+            </h3>
+            <div className="search-wrapper" >
+            <span><i class="fas fa-user"></i></span>
+            <input type="search" placeholder="Search here"></input>
+            </div>
+          
+            <div className="user-wrapper" >
+            <img src="./" width="40px" height="40px" alt="account-holder"></img>
+            <div>
+              <h4>Natur Gangs</h4>
+              <small>Super Admin</small>
+
+            </div>
+            </div>
+
+          </div>
+        </header>
+
+        <main>
+            <div className="cards">
+              <div className="card-single">
+                <div>
+                  <h1>54</h1>
+                  <span>Costumer</span><i class="fas fa-user"></i>
+                </div>
+              </div>
+              <div className="card-single">
+                <div>
+                  <h1>54</h1>
+                  <span>Projects</span><i class="fas fa-user"></i>
+                </div>
+              </div>
+              <div className="card-single">
+                <div>
+                  <h1>54</h1>
+                  <span>Orders</span><i class="fas fa-user"></i>
+                </div>
+              </div>
+              <div className="card-single">
+                <div>
+                  <h1>54</h1>
+                  <span>Income</span><i class="fas fa-user"></i>
+                </div>
+              </div>
+            </div>
+        </main>
+
+      </div>
     </>
     
   )
