a37745d1066ce0a9f22d585ede2165d0c9588576
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-09-22 23:59:27 +0200

    my account page continued to be styled

--- crm-frontend/src/pages/myaccount/MyAccount.js
+++ crm-frontend/src/pages/myaccount/MyAccount.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./MyAccount.css"
 
 
-const MyAccount = () =>  {
+const MyAccount = () => {
 
   return (
 
@@ -13,92 +13,93 @@ const MyAccount = () =>  {
         </div>
 
         <div className="sidebar-menu">
-        <ul>
-          <li>
-            <a href="./"> <i class="fas fa-user"></i>Dashboard</a>
-          </li>
-          <li>
-            <a href="./"> <span><i class="fas fa-user"></i></span>Costumers</a>
-          </li>
-          <li>
-            <a href="./"> <span><i class="fas fa-user"></i></span>Projects</a>
-          </li>
-          <li>
-            <a href="./"> <span><i class="fas fa-user"></i></span>Orders</a>
-          </li>
-          <li>
-            <a href="./"> <span><i class="fas fa-user"></i></span>Inventory</a>
-          </li>
-          <li>
-            <a href="./"> <span><i class="fas fa-user"></i></span>Accounts</a>
-          </li>
-          <li>
-            <a href="./"> <span><i class="fas fa-user"></i></span>Tasks</a>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <a href="./" className="active"><span><i class="fas fa-user"></i></span>Dashboard </a>
+            </li>
+            <li>
+              <a href="./"> <span><i class="fas fa-user"></i></span>Costumers</a>
+            </li>
+            <li>
+              <a href="./"> <span><i class="fas fa-user"></i></span>Projects</a>
+            </li>
+            <li>
+              <a href="./"> <span><i class="fas fa-user"></i></span>Orders</a>
+            </li>
+            <li>
+              <a href="./"> <span><i class="fas fa-user"></i></span>Inventory</a>
+            </li>
+            <li>
+              <a href="./"> <span><i class="fas fa-user"></i></span>Accounts</a>
+            </li>
+            <li>
+              <a href="./"> <span><i class="fas fa-user"></i></span>Tasks</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      </div>
-
-      
 
       <div className="main-content">
+
         <header>
+
           <div className="header-title">
             <h3>
-            <label for="">
-              <span><i class="fas fa-user"></i></span>
-            </label>
-            Dashboard
+              <label for="">
+                <span><i class="fas fa-user"></i></span>
+              </label>
+              Dashboard
             </h3>
-            <div className="search-wrapper" >
+          </div>
+          <div className="search-wrapper" >
             <span><i class="fas fa-user"></i></span>
             <input type="search" placeholder="Search here"></input>
-            </div>
-          
-            <div className="user-wrapper" >
-            <img src="./" width="40px" height="40px" alt="account-holder"></img>
+          </div>
+
+          <div className="user-wrapper" >
+            <img src="./images/img-3.svg" width="40px" height="40px" alt="account-holder"></img>
             <div>
               <h4>Natur Gangs</h4>
               <small>Super Admin</small>
 
             </div>
-            </div>
-
           </div>
+
+
         </header>
 
         <main>
-            <div className="cards">
-              <div className="card-single">
-                <div>
-                  <h1>54</h1>
-                  <span>Costumer</span><i class="fas fa-user"></i>
-                </div>
-              </div>
-              <div className="card-single">
-                <div>
-                  <h1>54</h1>
-                  <span>Projects</span><i class="fas fa-user"></i>
-                </div>
-              </div>
-              <div className="card-single">
-                <div>
-                  <h1>54</h1>
-                  <span>Orders</span><i class="fas fa-user"></i>
-                </div>
-              </div>
-              <div className="card-single">
-                <div>
-                  <h1>54</h1>
-                  <span>Income</span><i class="fas fa-user"></i>
-                </div>
+          <div className="cards">
+            <div className="card-single">
+              <div>
+                <h1>54</h1>
+                <span>Costumer</span><i class="fas fa-user"></i>
               </div>
             </div>
+            <div className="card-single">
+              <div>
+                <h1>54</h1>
+                <span>Projects</span><i class="fas fa-user"></i>
+              </div>
+            </div>
+            <div className="card-single">
+              <div>
+                <h1>54</h1>
+                <span>Orders</span><i class="fas fa-user"></i>
+              </div>
+            </div>
+            <div className="card-single">
+              <div>
+                <h1>54</h1>
+                <span>Income</span><i class="fas fa-user"></i>
+              </div>
+            </div>
+          </div>
         </main>
 
       </div>
     </>
-    
+
   )
 }
 
